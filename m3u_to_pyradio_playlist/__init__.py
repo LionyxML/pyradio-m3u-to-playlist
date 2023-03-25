0ce630dcfcb62aba0884c5c46f15f34902ab75e8
LionyxML <rahul.juliato@gmail.com>
2023-03-24 21:46:54 -0300

    feat: improves the setup.py

--- m3u_to_pyradio_playlist/__init__.py
+++ m3u_to_pyradio_playlist/__init__.py
@@ -1,6 +1,12 @@
-"""Init file."""
+" m3u_to_pyradio -- File converter to pyradio. "
 
-__version__ = "0.0.1"
+version_info = (0, 0, 1)
+
+__version__ = version = ".".join(map(str, version_info))
+__project__ = __name__
+__author__ = "Rahul M. Juliato"
+__license__ = "GPL-2.0"
+
 
 # Set default encoding to utf-8
 import sys
